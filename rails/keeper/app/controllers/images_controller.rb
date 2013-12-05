e847036dd93490f2a06f8b20a0470bad469bda59
aritzg <galdos.aritz@gmail.com>
2013-12-05 18:34:55 +0100

    Disabled csrf

--- rails/keeper/app/controllers/images_controller.rb
+++ rails/keeper/app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery except: :destroy
 
   # GET /images
   # GET /images.json
